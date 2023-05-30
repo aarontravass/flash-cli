ebfa529b29b1fd9fad89f9faaa002eea65721ab5
v1rtl <yo@v1rtl.site>
2023-05-30 16:12:33 +0300

    don't wait

--- api/functions.ts
+++ api/functions.ts
@@ -8,7 +8,7 @@ export const deployWeb3Function = async (name: string, path: string) => {
   const cid = await Web3FunctionBuilder.deploy(path)
   console.log(`Deploying ${name} on ipfs://${cid}`)
   try {
-    const res = await fetch('http://localhost:3000/api/deploy', {
+    const res = await fetch('https://create-gelato-w3f.vercel.app/api/deploy', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ cid, name }),
@@ -18,7 +18,7 @@ export const deployWeb3Function = async (name: string, path: string) => {
       console.log(`Success! ${name}-${taskId} -> ${tx.hash}`)
     } else {
       const err = await res.text()
-      return console.error(kleur.red(`Failed to deploy ${name}`))
+      return console.error(kleur.red(`Failed to deploy ${name}\n${err}`))
     }
   } catch (e) {
     return console.error(kleur.red(`Failed to deploy: ${e.message}`))
