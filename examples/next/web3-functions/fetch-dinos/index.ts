c2e7cd62791ef08ca01d16b082e92af5a6db1214
v1rtl <yo@v1rtl.site>
2023-05-31 10:26:35 +0300

    rewrite func

--- examples/next/web3-functions/fetch-dinos/index.ts
+++ examples/next/web3-functions/fetch-dinos/index.ts
@@ -15,14 +15,11 @@ await db.applySchema(`
 
 Web3Function.onRun(async ({ storage, multiChainProvider }) => {
   const lastBlock = parseInt((await storage.get('lastBlockNumber')) ?? '0')
-
   const newBlock = await multiChainProvider.default().getBlockNumber()
   if (newBlock > lastBlock) await storage.set('lastBlockNumber', newBlock.toString())
 
-  const res = await fetch(
-    'https://dinoipsum.com/api/?format=text&paragraphs=1&words=1', { method: 'GET' }
-  )
-  await db.collection('Dino').create([newBlock.toString(), (await res.text()).trim()])
-
-  return { canExec: false, message: 'Saved dino' }
+  const res = await fetch('https://dinoipsum.com/api/?format=text&paragraphs=1&words=1')
+  const dino = (await res.text()).trim()
+  await db.collection('Dino').create([newBlock.toString(), dino])
+  return { canExec: false, message: `Saved dino ${dino}` }
 })
