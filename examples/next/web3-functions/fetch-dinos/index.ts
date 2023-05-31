99c64040aba0d3f970abb225f7528eb25987ae96
v1rtl <yo@v1rtl.site>
2023-05-31 10:40:13 +0300

    try to change the function

--- examples/next/web3-functions/fetch-dinos/index.ts
+++ examples/next/web3-functions/fetch-dinos/index.ts
@@ -21,5 +21,5 @@ Web3Function.onRun(async ({ storage, multiChainProvider }) => {
   const res = await fetch('https://dinoipsum.com/api/?format=text&paragraphs=1&words=1')
   const dino = (await res.text()).trim()
   await db.collection('Dino').create([newBlock.toString(), dino])
-  return { canExec: false, message: `Saved dino ${dino}` }
+  return { canExec: true, message: `Saved dino ${dino}` , callData: [] }
 })
